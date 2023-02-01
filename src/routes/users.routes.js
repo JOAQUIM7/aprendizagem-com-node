30d3e36307dd7cab4430aaa46e483c959087a5d7
joaquim <joaquim.mesdesjr@gmail.com>
2023-02-01 15:40:15 -0300

    first commit

---
+++ src/routes/users.routes.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+
+const UsersControllers = require("../controllers/UsersController");
+
+const usersRoutes = Router();
+
+const usersControllers = new UsersControllers();
+
+usersRoutes.post("/", usersControllers.create);
+usersRoutes.put("/:id", usersControllers.update);
+
+module.exports = usersRoutes;
