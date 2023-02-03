1aa662c4760c767be7e9424baa1921b8bb7ebb8c
joaquim <joaquim.mesdesjr@gmail.com>
2023-02-02 21:43:14 -0300

    finalizando o backend

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -4,8 +4,11 @@ const NotesController = require("../controllers/NotesController");
 
 const notesRoutes = Router();
 
-const NotesController = new NotesController();
+const notesController = new NotesController();
 
-notesRoutes.post("/:user_id", NotesController.create);
+notesRoutes.get("/", notesController.index);
+notesRoutes.post("/:user_id", notesController.create);
+notesRoutes.get("/:id", notesController.show);
+notesRoutes.delete("/:id", notesController.delete);
 
 module.exports = notesRoutes;
